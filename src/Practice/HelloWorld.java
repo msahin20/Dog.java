d266d34af3df01ebbe3347d6ae9f225050c72dd8
msahin <mrtsahin79@gmail.com>
2022-01-23 11:21:15 -0500

    initial commit

--- src/Practice/HelloWorld.java
+++ src/Practice/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("NEvin naber");
+        System.out.println("_____________________");
 
         System.out.println();
 
